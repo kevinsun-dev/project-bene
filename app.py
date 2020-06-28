826ba0bda64e3b0f2f3b0e4bb781173ec096c528
Rachel Lu <lurachel18@gmail.com>
2020-06-28 10:56:55 -0400

    Get form data

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/',methods=['GET', 'POST'])
 def index():
     return "Hello there!"
@@ -12,5 +13,14 @@ def get_current_time():
     return {'time': time.time()}
 
 @app.route('/form',methods=['POST'])
-def index():
-    return request
+def form():
+    data = request.get_json()
+    answers = data["form_response"]["answers"]
+    interests = answers[0]["choices"].get("labels")
+    skills = answers[1]["choices"].get("labels")
+    userinfo = {"interests": interests, "skills": skills}
+    print(userinfo)
+    return userinfo
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
